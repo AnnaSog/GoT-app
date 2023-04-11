586808682476d167a1513d2147c397d4cd703276
Vartanush <anna87-07@inbox.ru>
2023-04-11 13:10:58 +0300

    build

---
+++ build/precache-manifest.5c31a59639e30795bea66641a896ad00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "57c06d2a73909f15a607",
+    "url": "/static/css/main.fe5336a8.chunk.css"
+  },
+  {
+    "revision": "57c06d2a73909f15a607",
+    "url": "/static/js/main.57c06d2a.chunk.js"
+  },
+  {
+    "revision": "f246903e08ee30b2bacf",
+    "url": "/static/css/1.0f3b5e9b.chunk.css"
+  },
+  {
+    "revision": "f246903e08ee30b2bacf",
+    "url": "/static/js/1.f246903e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "eb23bd5ff84135344aec23709c371559",
+    "url": "/index.html"
+  }
+];
